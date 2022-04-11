2e9c8bdfba6d6ddcdbc0c4663ef4ddf14278d6fc
Emir Güvenni <contact@emirguvenni.com>
2022-04-11 13:23:15 +0300

    Updated the migration file

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,14 @@
 require('dotenv').config();
 
 const contract_1 = artifacts.require(process.env.CONTRACT_1_NAME);
+const contract_2 = artifacts.require(process.env.CONTRACT_2_NAME);
 
 module.exports = function (deployer) {
-  deployer.deploy(contract_1, process.env.CONTRACT_1_TOKEN_SUPPLY);
+  deployer.deploy(
+    contract_1,
+    process.env.CONTRACT_1_TOKEN_NAME,
+    process.env.CONTRACT_1_TOKEN_SYMBOL,
+    process.env.CONTRACT_1_TOKEN_SUPPLY
+  );
+  deployer.deploy(contract_2, process.env.CONTRACT_2_TOKEN_SUPPLY);
 };
